30e7d792151471c098fecbc97283e8b98da4a810
Kiran_Joy <51417521+Kiran88joy@users.noreply.github.com>
2020-06-08 16:19:28 +0530

    Update 271A - Beautiful Year.cpp

--- 271A - Beautiful Year.cpp
+++ 271A - Beautiful Year.cpp
@@ -1,35 +1,4 @@
-// good code
 
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-
-#ifndef ONLINE_JUDGE
-	freopen("input.txt","r",stdin);
-	freopen("output.txt","w",stdout);
-#endif
-
-	int y;
-	cin>>y;
-	
-	while(true){
-		y++;
-		int a = y/1000;
-		int b = y/100 % 10;
-		int c = y/10 % 10;
-		int d = y%10;
-		if(a!=b&&a!=c&&a!=d&&b!=c&&b!=d&&c!=d){
-			cout<<y;
-			break;
-		}
-	}
-	return 0;
-
-}
-
-
-// ok code
 #include<bits/stdc++.h>
 using namespace std;
  
